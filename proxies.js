9ff7b2a00371cf1552a5450af3f9056a8025d575
Theo Sun <theo.sun@outlook.com>
2019-05-21 21:57:07 +0800

    chore(remove): unused files

--- proxies.js
+++ proxies.js
@@ -4,32 +4,21 @@ module.exports =
   /**
    * modify here to set node proxy server
    *
-   * documention see https://github.com/chimurai/http-proxy-middleware
+   * documentation see https://github.com/chimurai/http-proxy-middleware
    */
   [
     proxy("/resources", {
       target: "https://openui5.hana.ondemand.com",
       changeOrigin: true,
-      onProxyRes: function (p) {
+      onProxyRes: function(p) {
         p.headers["cache-control"] = "public, max-age=31536000";
       }
     }),
     proxy("/test-resources", {
       target: "https://openui5.hana.ondemand.com",
       changeOrigin: true,
-      onProxyRes: function (p) {
+      onProxyRes: function(p) {
         p.headers["cache-control"] = "public, max-age=31536000";
       }
-    }),
-    // Above proxies are not necessary
-
-    proxy("/destinations/northwind", {
-      target: "https://services.odata.org/",
-      // with http basic auth if necessary
-      auth: "username:password",
-      pathRewrite: {
-        '^/destinations/northwind': '/'
-      },
-      changeOrigin: true
     })
   ];
