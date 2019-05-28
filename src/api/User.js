4adf1ad55fc1c12721cdc8cad3e1cd6a2f0d760a
Theo Sun <theo.sun@sap.com>
2019-05-28 17:05:10 +0800

    chore(user): api type

--- src/api/User.js
+++ src/api/User.js
@@ -1,10 +1,28 @@
 import fetch from "unfetch";
 
+interface User {
+  ID: number;
+  CreatedAt: Date;
+  UpdatedAt: Date;
+  DeletedAt: null;
+  Name: string;
+  Description: string;
+  CreatedBy: null;
+  UpdatedBy: null;
+  FederationLoginID: string;
+  Email: string;
+  Tenants: null;
+}
+
+interface CurrentUserResponse {
+  User: User;
+}
+
 /**
  * fetch user information
  */
-export const fetchCurrentUserInformation = async() => {
-  var res = await fetch("/api/v1/user");
+export const fetchCurrentUserInformation = async(): Promise<CurrentUserResponse> => {
+  var res = await fetch("/api/v1/user/");
   var body = await res.json();
   return body;
 };
